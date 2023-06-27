692cc996eee804073e3db643154873d32d5753f0
cvilanova <carolinavilanova@gmail.com>
2023-06-27 17:44:54 -0300

    exercicios do beecrowd

---
+++ beecrowd.py
@@ -0,0 +1,42 @@
+#1000
+print("Hello World!")
+
+#1001
+a = int(input())
+b = int(input())
+X = a + b
+print('X = %i'%X)
+
+#1002
+raio = float(input())
+n = 3.14159
+area = n * raio**2
+print("A={:.4f}".format(area))
+
+#1003
+a=int(input())
+b=int(input())
+print("SOMA =",a+b)
+
+#1004
+a=int(input())
+b=int(input())
+print("PROD =", a*b)
+
+#1005
+A = float(input())
+B = float(input())
+media = (A*3.5 + B*7.5) / 11
+print("MEDIA = %.5f" % media)
+
+#1006
+A = float(input())
+B = float(input())
+C = float(input())
+media = (A*2 + B*3 + C*5) / 10
+print("MEDIA = %.1f" % media)
+
+
+
+
+
