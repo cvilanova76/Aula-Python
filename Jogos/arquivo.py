7e3b9ebeedfe4e652bfb66a58ed8bbbdb1ba888a
Carolina Carvalho Vila Nova <121834343+cvilanova76@users.noreply.github.com>
2023-07-08 15:12:52 -0300

    criando arquivo externo para jogo da forca

---
+++ Jogos/arquivo.py
@@ -0,0 +1,9 @@
+arquivo = open("palavras.txt", 'w')
+
+arquivo.write("botafogo\n")
+arquivo.write("vasco\n")
+arquivo.write("galo\n")
+arquivo.write("madureira\n")
+arquivo.write("america\n")
+
+arquivo.close()
